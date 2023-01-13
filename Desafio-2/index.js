f276a39cde98b74d338da676aacf083861a612aa
javier maita <javiermaita@hotmail.com.ar>
2023-01-13 17:32:48 -0300

    Desafio-2 13/1/2023

--- Desafio-2/index.js
+++ Desafio-2/index.js
@@ -25,8 +25,6 @@ class ProductManager {
                 product.id = dataJS[dataJS.length - 1].id + 1             //agrego id
                 dataJS.push(product)
 
-                console.log(product)
-
                 await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')        //se escribe en el archivo los productos en JSON
 
             }else{                           //si el archivo NO existe se crea uno
@@ -66,12 +64,39 @@ class ProductManager {
         }
     }
 
-    async updateProduct(){
+    async updateProduct(id, obj){
+        try {
+            let data = await fs.promises.readFile(this.path, 'utf-8')
+            let dataJS = JSON.parse(data)
 
+            let productById = dataJS.find(product => product.id == id)
+
+            productById = obj
+            productById.id = id
+
+            dataJS.splice((id - 1), 1, productById)
+
+            console.log(dataJS)
+
+            await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
+        } catch (error) {
+            return console.log(error);
+        }
     }
 
-    async deleteProduct(){
-        
+    async deleteProduct(id){
+        let data = await fs.promises.readFile(this.path, 'utf-8')
+        let dataJS = JSON.parse(data)
+
+        dataJS.splice((id - 1), 1)
+
+        let contador = 1
+
+        dataJS.forEach(product => {
+            product.id = contador++
+        })
+
+        await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
     }
 }
 
@@ -88,20 +113,37 @@ const productManager = new ProductManager()
 
 /// DE ESTA FORMA FUNCIONA -------------------------------------------------------------
 
+let remplazo =  {
+                    title: "Remplazo",
+                    description: "obj actualizado",
+                    price: 500,
+                    thumbnail: "link",
+                    code : 200,
+                    stock: 15
+                }
+
 productManager.addProduct("titulo", "es una prueba", 500, "link", 200, 15)
 
 setTimeout(()=>{
     productManager.addProduct("titulo 2", "es una prueba", 500, "link", 200, 15)
-},1000)
+},100)
 
 setTimeout(()=>{
     productManager.addProduct("titulo 3", "es una prueba", 500, "link", 200, 15)
-},2000)
+},200)
 
 setTimeout(()=>{
     productManager.getProducts()
-},3000)
+},300)
 
 setTimeout(()=>{
     productManager.getProductById(2)
-},4000)
+},400)
+
+setTimeout(()=>{
+    productManager.updateProduct(1, remplazo)
+},500)
+
+setTimeout(()=>{
+    productManager.deleteProduct(4)
+},600)
