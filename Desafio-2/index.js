a2d2f63b2afec317773e97f4a219f88350e2274f
javier maita <javiermaita@hotmail.com.ar>
2023-01-13 22:29:48 -0300

    Desafio-2-Completo

--- Desafio-2/index.js
+++ Desafio-2/index.js
@@ -1,91 +1,76 @@
 const fs = require('fs')
 
 class ProductManager {
-
+    #ruta = './products.json'
     constructor(){
-        this.path = './products.json'
+        this.path = this.#ruta
     }
 
-    async addProduct(title, description, price, thumbnail, code, stock){
-        try{
-            const product = {
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock
-            }
-            
-            if(fs.existsSync(this.path)){     //si el archivo existe se pushea el producto
-
-                let data = await fs.promises.readFile(this.path, 'utf-8') //data en JSON
-                let dataJS = JSON.parse(data)                             //data en JS
-
-                product.id = dataJS[dataJS.length - 1].id + 1             //agrego id
-                dataJS.push(product)
-
-                await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')        //se escribe en el archivo los productos en JSON
-
-            }else{                           //si el archivo NO existe se crea uno
-                product.id = 1
-                const arrProducts = [product]
-
-                await fs.promises.writeFile(this.path, `${JSON.stringify(arrProducts, null, 2)}`, 'utf-8')   //se crea el archivo con el producto en JSON
-            }
+    addProduct(title, description, price, thumbnail, code, stock){
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
         }
-        catch(err) {
-            return console.log(err);
+        
+        if(fs.existsSync(this.path)){     //si el archivo existe se pushea el producto
+            console.log("existe el archivo");
+            let data = fs.readFileSync(this.path, 'utf-8') //data en JSON
+            let dataJS = JSON.parse(data)                             //data en JS
+
+            product.id = dataJS[dataJS.length - 1].id + 1             //agrego id
+            dataJS.push(product)
+
+            fs.writeFileSync(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')        //se escribe en el archivo los productos en JSON
+
+        }else{                           //si el archivo NO existe se crea uno
+            console.log("NO existe el archivo");
+            product.id = 1
+            const arrProducts = [product]
+
+            fs.writeFileSync(this.path, `${JSON.stringify(arrProducts, null, 2)}`, 'utf-8')   //se crea el archivo con el producto en JSON
         }
     }
 
-    async getProducts(){
-        try{
-            let data = await fs.promises.readFile(this.path, 'utf-8')
-            let dataJS = JSON.parse(data)
+    getProducts(){
+        let data = fs.readFileSync(this.path, 'utf-8')
+        let dataJS = JSON.parse(data)
 
-            return console.log(dataJS);
-        }
-        catch(err) {
-            return console.log(err);
-        }
+        console.log(dataJS)
+        return dataJS
     }
 
-    async getProductById(id){
-        try {
-            let data = await fs.promises.readFile(this.path, 'utf-8')
-            let dataJS = JSON.parse(data)
+    getProductById(id){
+        let data = fs.readFileSync(this.path, 'utf-8')
+        let dataJS = JSON.parse(data)
 
-            const productById = dataJS.find(product => product.id == id)
+        const productById = dataJS.find(product => product.id == id)
 
-            return console.log(productById);
-        } catch (error) {
-            return console.log(error);
-        }
+        console.log(productById)
+        return productById
     }
 
-    async updateProduct(id, obj){
-        try {
-            let data = await fs.promises.readFile(this.path, 'utf-8')
-            let dataJS = JSON.parse(data)
+    updateProduct(id, obj){
+        let data = fs.readFileSync(this.path, 'utf-8')
+        let dataJS = JSON.parse(data)
 
-            let productById = dataJS.find(product => product.id == id)
+        let productById = dataJS.find(product => product.id == id)
 
-            productById = obj
-            productById.id = id
+        productById = obj
+        productById.id = id
 
-            dataJS.splice((id - 1), 1, productById)
+        dataJS.splice((id - 1), 1, productById)
 
-            console.log(dataJS)
+        console.log(dataJS)
 
-            await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
-        } catch (error) {
-            return console.log(error);
-        }
+        fs.writeFileSync(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
     }
 
-    async deleteProduct(id){
-        let data = await fs.promises.readFile(this.path, 'utf-8')
+    deleteProduct(id){
+        let data = fs.readFileSync(this.path, 'utf-8')
         let dataJS = JSON.parse(data)
 
         dataJS.splice((id - 1), 1)
@@ -96,22 +81,11 @@ class ProductManager {
             product.id = contador++
         })
 
-        await fs.promises.writeFile(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
+        fs.writeFileSync(this.path, `${JSON.stringify(dataJS, null, 2)}`, 'utf-8')
     }
 }
 
 const productManager = new ProductManager()
-
-/// DE ESTA FORMA NO FUNCIONA -------------------------------------------------------------
-
-// productManager.addProduct("titulo", "es una prueba", 500, "link", 200, 15)
-// productManager.addProduct("titulo 2", "es una prueba", 500, "link", 200, 15)
-// productManager.addProduct("titulo 3", "es una prueba", 500, "link", 200, 15)
-// productManager.getProducts()
-// productManager.getProductById(2)
-
-
-/// DE ESTA FORMA FUNCIONA -------------------------------------------------------------
 
 let remplazo =  {
                     title: "Remplazo",
@@ -123,27 +97,11 @@ let remplazo =  {
                 }
 
 productManager.addProduct("titulo", "es una prueba", 500, "link", 200, 15)
-
-setTimeout(()=>{
-    productManager.addProduct("titulo 2", "es una prueba", 500, "link", 200, 15)
-},100)
-
-setTimeout(()=>{
-    productManager.addProduct("titulo 3", "es una prueba", 500, "link", 200, 15)
-},200)
-
-setTimeout(()=>{
-    productManager.getProducts()
-},300)
-
-setTimeout(()=>{
-    productManager.getProductById(2)
-},400)
-
-setTimeout(()=>{
-    productManager.updateProduct(1, remplazo)
-},500)
-
-setTimeout(()=>{
-    productManager.deleteProduct(4)
-},600)
+productManager.addProduct("titulo 2", "es una prueba", 500, "link", 200, 15)
+productManager.addProduct("titulo 3", "es una prueba", 500, "link", 200, 15)
+productManager.addProduct("titulo 4", "es una prueba", 500, "link", 200, 15)
+productManager.addProduct("titulo 5", "es una prueba", 500, "link", 200, 15)
+productManager.getProducts()
+productManager.getProductById(3)
+productManager.updateProduct(1, remplazo)
+productManager.deleteProduct(2)
